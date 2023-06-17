1f99fc806245b3398351e5591e144808c7154307
brandonayala28 <brandonayala4328@icloud.com>
2023-06-17 12:21:46 -0500

    exercises

---
+++ exercise.java
@@ -0,0 +1,121 @@
+public class exercise  {
+
+    // public static void main(String[] args) {
+        
+    //     // Lisa is cooking muffins. The recipe calls for 7 cups of sugar. She has already put in 2 cups. 
+    //     // How many more cups does she need to put in?
+    //         int maxCups = 7;
+    //         int lisaCups = 2;
+
+    //         int totalNumberOfCupsNeeded;
+
+    //         totalNumberOfCupsNeeded = maxCups - lisaCups;
+
+    //         System.out.println("Total number of cups needed: " + totalNumberOfCupsNeeded );
+
+    // }
+        
+        // At a restaurant, Mike and his three friends decided to divide the bill evenly. If each person 
+        // paid $13 then what was the total bill?
+
+
+//         public static void main(String[] args) {
+//         int numberOfPeople = 4;
+//         int amountPaidByEachPerson = 13.0;
+
+//         int totalBill = numberOfPeople * amountPaidByEachPerson;
+
+//         System.out.println("The total bill: $" + totalBill);
+//     }
+// }
+
+        // How many packages of diapers can you buy with $40 if one package costs $8?
+
+//             public static void main(String[] args) {
+//             int budget = 40;
+//             int packageCost = 8;
+//             int numberOfPackages = (int) (budget / packageCost);
+//             System.out.println("You can buy " + numberOfPackages + " packages of diapers.");
+//     }
+// }
+
+
+        //Last Friday Trevon had $29. Over the weekend he received some money for cleaning the attic. He 
+        // now has $41. How much money did he receive?
+
+        // Last week Julia ran 30 miles more than Pranav. Julia ran 47 miles. How many miles did Pranav run?
+            
+//     public static void main(String[] args) {
+//         int juliaDistance = 47; // Julia's distance in miles
+//         int pranavDistance = juliaDistance - 30; // Pranav's distance is 30 miles less than Julia's
+        
+//         System.out.println("Pranav ran " + pranavDistance + " miles.");
+//     }
+// }
+        // How many boxes of envelopes can you buy with $12 if one box costs $3?
+
+    //     public static void main(String[] args) {
+    //     int money = 12; // Amount of money in dollars
+    //     int costPerBox = 3; // Cost of one box of envelopes in dollars
+        
+    //     int numberOfBoxes = (int) (money / costPerBox); // Calculate the number of boxes
+        
+    //     System.out.println("You can buy " + numberOfBoxes + " boxes of envelopes.");
+    // }}
+
+        // After paying $5.12 for a salad, Norachai has $27.10. How much money did he have before buying the salad?
+    //     public static void main(String[] args) {
+    //     double costOfSalad = 5.12; // Cost of the salad in dollars
+    //     double remainingMoney = 27.10; // Remaining amount of money after buying the salad
+        
+    //     double initialMoney = remainingMoney + costOfSalad; // Calculate the initial amount of money
+        
+    //     System.out.println("Norachai had $" + initialMoney + " before buying the salad.");
+    // }}
+        
+
+
+    // 331 students went on a field trip. Six buses were filled and 7 students traveled in cars. How many students were in each bus?
+    //     public static void main(String[] args) {
+    //     int totalStudents = 331; // Total number of students on the field trip
+    //     int cars = 7; // Number of students traveling in cars
+    //     int buses = 6; // Number of buses filled
+        
+    //     int studentsPerBus = (totalStudents - cars) / buses; // Calculate the number of students per bus
+        
+    //     System.out.println("There were " + studentsPerBus + " students in each bus.");
+    // }}
+// Aliyah had $24 to spend on seven pencils. After buying them she had $10. How much did each pencil cost?
+    //     public static void main(String[] args) {
+    //     int initialMoney = 24; // Initial amount of money in dollars
+    //     int finalMoney = 10; // Final amount of money after buying pencils
+    //     int numberOfPencils = 7; // Number of pencils purchased
+        
+    //     double costPerPencil = (initialMoney - finalMoney) / numberOfPencils; // Calculate the cost per pencil
+        
+    //     System.out.println("Each pencil costs $" + costPerPencil);
+    // }}
+// The sum of three consecutive numbers is 72. Print these numbers consecutively to the console.
+        // public static void main(String[] args) {
+        //     int sum = 72;
+            
+        // }
+        // }
+// The sum of three consecutive even numbers is 48. What is the smallest of these numbers?
+// 
+        public static void main(String[] args) {
+        int sum = 48;
+        }
+    }
+
+// Maria has boxes. She buys seven more. A week later, half of her boxes are destroyed in a fire, leaving her with 22 boxes. 
+// With how many did she start?
+    //     public static void main(String[] args) {
+    //     int finalBoxes = 22; // Final number of boxes Maria has
+    //     int boxesBought = 7; // Number of boxes Maria bought
+    //     int boxesDestroyed = finalBoxes / 2; // Number of boxes destroyed in the fire
+        
+    //     int initialBoxes = finalBoxes - boxesBought - boxesDestroyed; // Calculate the initial number of boxes
+        
+    //     System.out.println("Maria started with " + initialBoxes + " boxes.");
+    // }}
